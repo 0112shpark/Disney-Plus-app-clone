6f9fc6ac6563e9f77b16726842236cb37efa8843
0112shpark <000112shpark@naver.com>
2023-03-11 17:11:41 +0900

    :sparkles: add row components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Nav from "./components/Nav";
 import styled from "styled-components";
 import Banner from "./components/Banner";
 import Category from "./components/Category";
+import Row from "./components/Row";
+import requests from "./api/request";
 
 function App() {
   return (
@@ -11,6 +13,22 @@ function App() {
         <Nav />
         <Banner />
         <Category />
+        <Row
+          title="Trending Now"
+          id="TN"
+          fetchUrl={requests.fetchTrending}
+        ></Row>
+        <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}></Row>
+        <Row
+          title="Action Movies"
+          id="AM"
+          fetchUrl={requests.fetchActionMovies}
+        ></Row>
+        <Row
+          title="Comedy Movies"
+          id="CM"
+          fetchUrl={requests.fetchComedyMovies}
+        ></Row>
       </Container>
     </div>
   );
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,62 @@
+import axios from "../api/axios";
 import React from "react";
+import { useState, useEffect } from "react";
+import { useCallback } from "react";
+import "./Row.css";
 
-const Row = () => {
-  return <div>Row</div>;
+const Row = ({ title, id, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+
+  // usecallback 을 사용해 fetchUrl의 값이 변하지 않았으면
+  // 함수 재 생성X
+  const fetchMovieDate = useCallback(async () => {
+    const response = await axios.get(fetchUrl);
+    setMovies(response.data.results);
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fetchMovieDate();
+  }, [fetchMovieDate]);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
+        </div>
+
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.name}
+            ></img>
+          ))}
+          <div className="slider__arrow-right">
+            <span
+              className="arrow"
+              onClick={() => {
+                document.getElementById(id).scrollLeft +=
+                  window.innerWidth - 80;
+              }}
+            >
+              {">"}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Row;
