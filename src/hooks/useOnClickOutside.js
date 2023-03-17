6a6e680dd4f1c4bfceb7fa37acf5e50ef3b109d8
0112shpark <000112shpark@naver.com>
2023-03-17 18:58:04 +0900

    :zap: custom hooks for clicking outside of modal

---
+++ src/hooks/useOnClickOutside.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+export default function useOnClickOutside(ref, handler) {
+  useEffect(() => {
+    const listner = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      if (event.key !== "Escape") {
+        return;
+      }
+      handler();
+    };
+    document.addEventListener("mousedown", listner);
+    document.addEventListener("touchstart", listner);
+    document.addEventListener("keydown", listner);
+    return () => {
+      document.removeEventListener("mousedown", listner);
+      document.removeEventListener("touchstart", listner);
+      document.removeEventListener("keydown", listner);
+    };
+  }, [ref, handler]);
+}
